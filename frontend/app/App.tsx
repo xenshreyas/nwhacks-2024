c7fdf1012489f63827876fa43bebfb9f6222f771
graceco22 <132722611+graceco22@users.noreply.github.com>
2024-01-20 14:08:13 -0800

    grace

---
+++ frontend/app/App.tsx
@@ -0,0 +1,14 @@
+// App.tsx
+
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* Your component JSX goes here */}
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
+
+export default App;
